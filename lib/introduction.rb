614052ac088adfd1b2fddb7a541a0e271694bde4
Thomas Chilzer <tchilzer2@gmail.com>
2019-06-19 21:36:27 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,19 @@
+name = "Thomas"
+language="Ruby"
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name , language)
+  puts "Hi, my name is #{name} and I am learining to program in #{language}."
+end
+
+def introduction_with_language_optional(name , language)
+  puts "Hi, my name is #{name} and I am learining to program in #{language}."
+end
+  
+  introduction(name)
+  introduction_with_language("name" , "language")
+  introduction_with_language_optional(name , "Ruby")
+  
